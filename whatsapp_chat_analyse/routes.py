322cd442f703e787f3b04d61cdeb72a9ea450769
Vrutik Patwa <vrutikpatwa712@gmail.com>
2025-04-17 23:43:29 +0530

    Delete whatsapp_chat_analyse directory

--- whatsapp_chat_analyse/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask,render_template,redirect,url_for
-from whatsapp_chat_analyse import app
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('Home.html')
-
